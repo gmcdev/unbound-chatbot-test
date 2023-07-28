1255ad34282fc8d3503e32ae3b8ac41330ccabac
Greg Connell <gconnell@160over90.com>
2023-07-27 21:37:32 -0700

    shaping up firebase config for deploy

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
-import config from '../gcloud-service-account.json';
-
+// import config from '../gcloud-service-account.json';
 import { getApp, getApps, initializeApp } from 'firebase/app';
 
+const config = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+};
 const app = !getApps().length ? initializeApp(config) : getApp();
-console.log({ app });
 export default app;
